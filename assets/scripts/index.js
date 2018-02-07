08e7dc2f5a4e50ebe73f9f4aaf61fcac4d60cc90
jal90 <mezzyjezze@gmail.com>
2018-02-07 15:39:27 -0500

    Master commit so I can make new branches
    Master commit so I can make new branches

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -12,3 +12,52 @@ $(() => {
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
+
+const gameBoard = {
+  upLeft: '',
+  upCent: '',
+  upRight: '',
+  midLeft: '',
+  midCent: '',
+  midRight: '',
+  botLeft: '',
+  botCent: '',
+  botRight: ''
+}
+
+// out of date, use realMove instead
+const move = function (player, position) { // player either 'x' or 'o', position is key of gameBoard in quotes
+  if (gameBoard[position] === '') {
+    gameBoard[position] = player
+  } else {
+    console.log('Please pick an unoccupied space')
+  }
+}
+
+const changeTurns = function () {
+  const x = 'x'
+  const o = 'o'
+  let total = 0
+  for (const key in gameBoard) {
+    if (gameBoard[key] === '') {
+      ++total
+    }
+  }
+  if (total % 2 === 1) {
+    console.log("it's x's turn")
+    return x
+  } else if (total % 2 === 0) {
+    console.log("it's o's turn")
+    return o
+  } else {
+    console.log('uh oh')
+  }
+}
+
+const realMove = function (position) {
+  if (gameBoard[position] === '') {
+    gameBoard[position] = changeTurns()
+  } else {
+    console.log('Please pick an unoccupied space') // have this message Pop up in UI, not as alert, but in shiny red color?
+  }
+}
