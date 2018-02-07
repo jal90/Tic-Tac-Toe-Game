e14400ba69a696a7134e7ffc1b3bea846d3259b6
jal90 <mezzyjezze@gmail.com>
2018-02-07 18:14:24 -0500

    Add game logic and DOM manipulation
    Now there is one function that updates the gameBoard js object and shows
    selection on the screen.
    A second function (gameOver) checks if all values in the gameBoard
    object are non-empty strings
    
    A little feedback on the right-hand side

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -25,15 +25,6 @@ const gameBoard = {
   botRight: ''
 }
 
-// out of date, use realMove instead
-const move = function (player, position) { // player either 'x' or 'o', position is key of gameBoard in quotes
-  if (gameBoard[position] === '') {
-    gameBoard[position] = player
-  } else {
-    console.log('Please pick an unoccupied space')
-  }
-}
-
 const changeTurns = function () {
   const x = 'x'
   const o = 'o'
@@ -44,20 +35,55 @@ const changeTurns = function () {
     }
   }
   if (total % 2 === 1) {
-    console.log("it's x's turn")
+    // console.log("it's x's turn")
     return x
   } else if (total % 2 === 0) {
-    console.log("it's o's turn")
+    // console.log("it's o's turn")
     return o
   } else {
     console.log('uh oh')
   }
 }
 
-const realMove = function (position) {
-  if (gameBoard[position] === '') {
-    gameBoard[position] = changeTurns()
+const move = function (id) {
+  if (gameBoard[id] === '') {
+    $('#' + id).append(changeTurns())
+    gameBoard[id] = changeTurns()
   } else {
-    console.log('Please pick an unoccupied space') // have this message Pop up in UI, not as alert, but in shiny red color?
+    $('#feedback').html('PLEASE PICK EMPTY SPACE') // have this message Pop up in UI, not as alert, but in shiny red color?
+  }
+  console.log(gameBoard)
+}
+
+// logic has been incorporated into move function
+// const showMove = function () {
+//   $(this).append(changeTurns())
+// }
+
+// for seeing choice with mouse hover
+// const hoverOn = function () {
+//   $(this).html(changeTurns())
+// }
+// const hoverOff = function () {
+//   $(this).html('')
+// }
+
+const gameOver = function () {
+  let total = 0
+  for (const key in gameBoard) {
+    if (gameBoard[key] !== '') {
+      ++total
+    }
+  }
+  if (total === 9) {
+    $('#feedback').html('Game is Over!')
   }
 }
+
+$(() => {
+  // $('.col-xs-4').hover(hoverOn, hoverOff)  --> for when hoverOn and hoverOff work
+  // $('.col-xs-4').on('click', showMove)
+  $('.col-xs-4')
+    .on('click', function () { move($(this).attr('id')) })
+    .on('click', gameOver)
+})
