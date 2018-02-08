baee411fc30c5b39a0bb702789a6c3fe939b7b43
jal90 <mezzyjezze@gmail.com>
2018-02-08 14:49:05 -0500

    Create ui feedback for api stuff
    Created basic ui feedback for signing in, up, out, and chaning password.
    Will need to update to stuff that makes more sense, but I'll save that
    for when I'm really figuring out where/how my forms and buttons are
    presented.

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -143,6 +143,8 @@ const onSignUp = function (event) {
   event.preventDefault()
   const data = getFormFields(event.target)
   api.signUp(data)
+    .then(ui.signUpSuccess)
+    .catch(ui.signUpFailure)
 }
 
 const onSignIn = function (event) {
@@ -150,17 +152,21 @@ const onSignIn = function (event) {
   const data = getFormFields(event.target)
   api.signIn(data)
     .then(ui.signInSuccess)
+    .catch(ui.signInFailure)
 }
 
 const onSignOut = function (event) {
   event.preventDefault()
   api.signOut(store.data)
+    .then(ui.signOutSuccess)
 }
 
 const onChangePw = function (event) {
   event.preventDefault()
   const data = getFormFields(event.target)
   api.changePw(data)
+    .then(ui.changePasswordSuccess)
+    .catch(ui.changePasswordFailure)
 }
 
 const boardHandlers = () => {
--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -1,10 +1,54 @@
 const store = require('./store')
 
+const signUpSuccess = function (data) {
+  $('#sign-up').text('Signed up successfully')
+  $('#sign-up').css('background-color', 'green')
+  console.log(data)
+}
+
+const signUpFailure = function (error) {
+  $('#sign-up').text('Error on signing up')
+  $('#sign-up').css('background-color', 'red')
+  console.error(error)
+}
+
 const signInSuccess = function (data) {
   console.log(data)
+  $('#sign-in').text('Signed up successfully')
+  $('#sign-in').css('background-color', 'green')
   store.user = data.user
 }
 
+const signInFailure = function (error) {
+  $('#sign-in').text('Error on signing IN')
+  $('#sign-in').css('background-color', 'red')
+  console.error(error)
+}
+
+const signOutSuccess = function (data) {
+  $('#sign-in').text('signed out successfully')
+  $('#sign-in').css('background-color', 'purple')
+  console.log('signed out successfully')
+}
+
+const changePasswordSuccess = function (data) {
+  $('#change-pw').text('Changed password successfully')
+  $('#change-pw').css('background-color', 'green')
+  console.log('changed password successfully')
+}
+
+const changePasswordFailure = function (error) {
+  $('#change-pw').text('Error on changing password')
+  $('#change-pw').css('background-color', 'red')
+  console.error(error)
+}
+
 module.exports = {
-  signInSuccess
+  signUpSuccess,
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  signOutSuccess,
+  changePasswordSuccess,
+  changePasswordFailure
 }
